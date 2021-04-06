25844cff77c88a25b675814f6c55548f63c67c12
snayeri <68785737+snayeri@users.noreply.github.com>
2021-04-05 21:58:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # Needed to actually package something
     packages=['IBL'],
     # Needed for dependencies
-    install_requires=['numpy', 'pandas', 'pandas_datareader', 'scipy.optimize'],
+    install_requires=['numpy', 'pandas', 'pandas_datareader', 'scipy'],
     # *strongly* suggested for sharing
     version='0.1',
      # The license can be anything you like
