984986f8bad27953a656c774a72e77b8467301df
snayeri <68785737+snayeri@users.noreply.github.com>
2021-04-05 21:54:36 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import find_packages,setup
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='IBL',
-    url='https://github.com/snayeri/Portfolio_Management/Idzorek_Black_Litterman',
+    url='https://github.com/snayeri/Idzorek_Black_Litterman',
     author='Siamak Nayeri',
     author_email='siamaknayeri@gmail.com',
     # Needed to actually package something
-    packages=find_packages(),
+    packages=['IBL'],
+    # Needed for dependencies
+    install_requires=['numpy', 'pandas', 'pandas_datareader', 'scipy.optimize'],
+    # *strongly* suggested for sharing
+    version='0.1',
+     # The license can be anything you like
+    license='Liam_Nayeri',
     description='personal take on Idzorek Black Litterman'
