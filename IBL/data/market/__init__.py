bae3e945873c2a4719a6a22aacc628e30fff6b15
snayeri <68785737+snayeri@users.noreply.github.com>
2021-04-07 14:25:21 -0700

    Update __init__.py

--- IBL/data/market/__init__.py
+++ IBL/data/market/__init__.py
@@ -1,1 +1,15 @@
+from pandas import DataFrame
 
+from IBL.data.utility import load_file
+
+
+def load() -> DataFrame:
+
+    """
+    Load the prices for global market portoflio
+    Returns
+    -------
+    data : DataFrame
+        Data set containing prices for ACWI ETF, representing the market portfolio.
+    """
+    return load_file(__file__, "market.csv.gz")
